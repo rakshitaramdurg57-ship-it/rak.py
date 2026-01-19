31b7acfe354068601216a4ac643918f891b9067f
rakshitaramdurg57-ship-it <rakshitaramdurg57@gmail.com>
2026-01-19 15:26:58 +0530

    Create sm.py

---
+++ sm.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age:")
+        if age >18:
+        print(f"enter you are eligible:")
+        else:
+print(f"enter you are not eligible:")
+except value error:
+print(f"enter valid for a age:")
